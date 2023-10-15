4b6c5372dea83e2df11d42cb8668f743884d0405
QuasimKhan <quasimkhanpersonal@gmail.com>
2023-10-15 12:51:28 +0530

    Using Multiple Class "Function" and "Car" java

---
+++ Function.java
@@ -0,0 +1,9 @@
+public class Function {
+    public void FullThroatal() {
+        System.out.println("The Car is in Full throatal");
+    }
+
+    public void maxSpeed(int x) {
+        System.out.println("The Max speed of the car is : " + x + " KM/h.");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Function myObj = new Function();
+        myObj.FullThroatal();
+        myObj.maxSpeed(300);
+    }
+}
